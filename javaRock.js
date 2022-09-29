4805baa8aa472ba52b868ef1c7a186375db6601e
Lars <lars.kolankiewicz@gmail.com>
2022-09-29 21:25:03 +0200

    part 1

--- javaRock.js
+++ javaRock.js
@@ -1,1 +1,12 @@
 console.log('hello');
+
+/* part 1 */
+let choices = ['rock','paper','scissors'];
+
+function getComputerchoice(arr) {
+
+    ran = Math.floor(Math.random()* arr.length);
+    rann = arr[ran];
+    return rann;
+}
+console.log(getComputerchoice(choices));
